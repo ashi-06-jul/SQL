e71a4bdf5e17404586274b9cf210473c0fce906a
Ashi Anand <ashianand21@gmail.com>
2018-11-19 21:53:43 +0530

    sql

---
+++ SQLQuery2.sql
@@ -0,0 +1,13 @@
+select *from tblperson
+insert into tblperson values (10,'vatsal','v@v.com',1)
+alter table tblperson add salary int
+update tblperson set salary = '10000' where id=2
+update tblperson set salary = '20000' where id=3
+update tblperson set salary = '8000' where id=4
+update tblperson set salary = '50000' where id=5
+update tblperson set salary = '18000' where id=6
+update tblperson set salary = '70000' where id=7
+update tblperson set salary = '42000' where id=8
+update tblperson set salary = '64000' where id=9
+update tblperson set salary = '28000' where id=10
+update tblperson set salary = '90000' where id=1
