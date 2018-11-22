0daafcde1dfe4c71ae1ca48a402bf55744117793
Ashi Anand <ashianand21@gmail.com>
2018-11-22 23:14:01 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,4 +0,0 @@
-select * from tblperson
-select * from tblgender
-insert into tblgender (ID,GENDER)values (2,'female')
-insert into tblperson (ID,NAME,EMAIL,GENDERID,MobileNo,age) VALUES (11,'SAKUU','SS@',2,232,-10)
