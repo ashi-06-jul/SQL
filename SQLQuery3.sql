f946dabc6b2e6f09016784bb87ed752d88dbae0e
Ashi Anand <ashianand21@gmail.com>
2018-11-22 23:13:52 +0530

    sq

--- SQLQuery3.sql
+++
@@ -1,6 +0,0 @@
-select GENDERID,city,sum(salary) as totalsalary from tblperson group by GENDERID,city
-
-select city,sum(salary) as totalsalary from tblperson group by city having city = 'lucknow' 
-
-
-select * from tblperson
