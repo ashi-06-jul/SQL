29ac05561fca360fde4b127494299d902449a814
ashi-06-jul <36784332+ashi-06-jul@users.noreply.github.com>
2018-12-16 23:54:08 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,4 +2,3 @@ select * from tblperson
 select * from tblgender 
 insert into tblgender (ID,GENDER)values (2,'female')
 insert into tblperson (ID,NAME,EMAIL,GENDERID,MobileNo,age) VALUES (11,'SAKUU','SS@',2,232,-10)
-
